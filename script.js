43a1ba8d532fc7bfa2c6ccb75a8f00750a6e17c9
Fumeda <kubasliwa111@gmail.com>
2025-01-29 19:40:18 +0100

    bleh

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //use some kind of filereader for this?
 const titleMessages = [
-  "im stuck in the title of some lousy website",
   "Rhinestones beneath Shattered Glass",
   "That's that and this is this",
   "thanks for having js enabled",
@@ -8,16 +7,16 @@ const titleMessages = [
   "215211441251919221594",
   "i love eating cement",
   "check out femtanyl",
-  "Keep to myself.",
+  "Random Gods",
   "lorem ipsum",
+  "不幸な",
   "hi",
   "",
 ];
 
 //randomizes the title of the website
 function randomizeTitle() {
-  document.title =
-    titleMessages[Math.floor(Math.random() * titleMessages.length)];
+  document.title = titleMessages[Math.floor(Math.random() * titleMessages.length)];
 }
 
 //toggle between light and dark mode
